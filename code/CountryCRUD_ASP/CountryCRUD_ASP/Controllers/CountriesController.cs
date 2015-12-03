579c27398ecf531bd0681de7e63e10e14b761090
snitsars <sergsnicarenko@gmail.com>
2015-12-04 01:48:09 +0200

    Added print items on WebApi

---
+++ code/CountryCRUD_ASP/CountryCRUD_ASP/Controllers/CountriesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CountryCRUD_ASP.Models;
+
+namespace CountryCRUD_ASP.Controllers
+{
+    public class CountriesController : ApiController
+    {
+        private readonly Country[] _countries = new Country[]
+        {
+            new Country { Id = new Guid("CA72535B-F50A-4525-B536-BD0547D0902E"), Name = "Japan" },
+            new Country { Id = new Guid("E8FE299D-A0FA-4775-8EE1-F79FDEEADB91"), Name = "Ukraine" },
+            new Country { Id = new Guid("403B4356-DD37-49AA-94CD-C7336DF31FB8"), Name = "USA"}
+        };
+
+        public IEnumerable<Country> GetAllProducts()
+        {
+            return _countries;
+        }
+
+
+        public IHttpActionResult GetCountries(Guid id)
+        {
+            var country = _countries.FirstOrDefault((p) => p.Id == id);
+            if (_countries == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+    }
+}
--- code/CountryCRUD_ASP/CountryCRUD_ASP/Models/Country.cs
+++ code/CountryCRUD_ASP/CountryCRUD_ASP/Models/Country.cs
@@ -9,6 +9,7 @@ namespace CountryCRUD_ASP.Models
 {
     public class Country
     {
-
+        public Guid Id { get; set; }
+        public string Name { get; set; }
     }
 }
